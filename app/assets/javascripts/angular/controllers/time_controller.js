753e3dd05f307a7505f5fc35338b11d04da1de19
Dunya Kirkali <dunyakirkali@localhost.localdomain>
2014-04-19 23:03:41 +0200

    Improved timer ctlr

--- app/assets/javascripts/angular/controllers/time_controller.js
+++ app/assets/javascripts/angular/controllers/time_controller.js
@@ -1,5 +1,30 @@
 sgApp.controller('TimeController', ['$scope',
-function ($scope) {  
-  
+function ($scope) {
+  $scope.speed = 50; // km/h
+  $scope.state = 'paused';
+
+
+  $scope.play = function() {
+    $scope.state = 'playing';
+    $scope.start_timer();
+  }
+  $scope.pause = function() {
+    $scope.state = 'paused';
+    $scope.stop_timer();
+  }
+
+  $scope.start_timer = function() {
+    $scope.start_time =  new Date();
+    $scope.time = 0;
+    $scope.timer = setInterval($scope.tick, 1000);
+  }
+  $scope.stop_timer = function() {
+    clearInterval($scope.timer);
+  }
+
+  $scope.tick = function() {
+    $scope.time = parseInt((new Date() - $scope.start_time) / 1000.0);
+    $scope.$apply();
+  }
 
 }]);
